96b44387056bc635bc5a08de067b9027d1f4d959
root <juferafo@hotmail.com>
2021-12-02 17:53:00 +0000

    StageToRedshiftOperator coded. COPY Not tested

--- dags/sparkify_dag.py
+++ dags/sparkify_dag.py
@@ -2,8 +2,11 @@ from datetime import datetime, timedelta
 import os
 from airflow import DAG
 from airflow.operators.dummy_operator import DummyOperator
-from airflow.operators import (StageToRedshiftOperator, LoadFactOperator,
-                                LoadDimensionOperator, DataQualityOperator)
+from airflow.operators import StageToRedshiftOperator
+from airflow.operators import LoadFactOperator
+from airflow.operators import LoadDimensionOperator
+from airflow.operators import DataQualityOperator
+
 from helpers import SqlQueries
 
 AWS_KEY = os.environ.get('AWS_KEY', None)
@@ -11,79 +14,39 @@ AWS_SECRET = os.environ.get('AWS_SECRET', None)
 
 default_args = {
     'owner': 'udacity',
-    'start_date': datetime(2019, 1, 12),
-    'schedule_interval': None 
+    'start_date': datetime(2019, 1, 12)
 }
 
-s3_log_data = 's3://udacity-dend/log_data'
-s3_song_data = 's3://udacity-dend/song_data'
+s3_staging_areas = {
+    'events': 's3://udacity-dend/log_data',
+    'songs': 's3://udacity-dend/song_data'
+}
+
+redshift_conn_id = 'redshift_conn_id'
+aws_conn_id = 'aws_credentials'
 
 with DAG(
     'sparkify_pipeline',
     default_args=default_args,
     description='Load and transform data in Redshift with Airflow',
+    schedule_interval=None
 ) as dag:
 
     start_operator = DummyOperator(task_id='Begin_execution', dag=dag)
 
-    stage_events_to_redshift = StageToRedshiftOperator(
-        task_id='Stage_events',
-        dag=dag
-    )
+    staging_operators = {}
+    for staging_area in s3_staging_areas.keys():
+        staging_operators[staging_area] = StageToRedshiftOperator(
+            task_id=f'Staging_{staging_area}_data',
+            dag=dag,
+            aws_conn_id=aws_conn_id,
+            redshift_conn_id=redshift_conn_id,
+            redshift_schema='public',
+            redshift_table=f'staging_{staging_area}',
+            s3_path=s3_staging_areas[staging_area],
+            mode='REPLACE'
+        )
 
-    stage_songs_to_redshift = StageToRedshiftOperator(
-        task_id='Stage_songs',
-        dag=dag
-    )
-
-    data_staging = [stage_events_to_redshift, stage_songs_to_redshift]
-
-    start_dwh_data_load = DummyOperator(task_id='Start_DWH_data_load', dag=dag)
-
-    load_songplays_table = LoadFactOperator(
-        task_id='Load_songplays_fact_table',
-        dag=dag
-    )
-
-    load_user_dimension_table = LoadDimensionOperator(
-        task_id='Load_user_dim_table',
-        dag=dag
-    )
-
-    load_song_dimension_table = LoadDimensionOperator(
-        task_id='Load_song_dim_table',
-        dag=dag
-    )
-
-    load_artist_dimension_table = LoadDimensionOperator(
-        task_id='Load_artist_dim_table',
-        dag=dag
-    )
-
-    load_time_dimension_table = LoadDimensionOperator(
-        task_id='Load_time_dim_table',
-        dag=dag
-    )
-
-    dwh_data_load = [
-        load_songplays_table, 
-        load_user_dimension_table, 
-        load_song_dimension_table, 
-        load_artist_dimension_table, 
-        load_time_dimension_table
-        ]
-
-    run_quality_checks = DataQualityOperator(
-        task_id='Run_data_quality_checks',
-        dag=dag
-    )
-
-    end_operator = DummyOperator(task_id='Stop_execution', dag=dag)
-
-    # pipeline flow
-    start_operator >> data_staging
-    data_staging >> start_dwh_data_load
-    start_dwh_data_load >> dwh_data_load
-    dwh_data_load >> run_quality_checks
-    end_operator
+        start_operator >> staging_operators[staging_area]
+        
 
--- plugins/operators/stage_redshift.py
+++ plugins/operators/stage_redshift.py
@@ -1,25 +1,78 @@
 from airflow.hooks.postgres_hook import PostgresHook
+from airflow.hooks.S3_hook import S3Hook
 from airflow.models import BaseOperator
 from airflow.utils.decorators import apply_defaults
+import logging
 
 class StageToRedshiftOperator(BaseOperator):
     ui_color = '#358140'
 
     @apply_defaults
-    def __init__(self,
-                 # Define your operators params (with defaults) here
-                 # Example:
-                 # redshift_conn_id=your-connection-name
-                 *args, **kwargs):
+    def __init__(
+        self,
+        aws_conn_id : str = None,
+        redshift_conn_id : str = None,
+        redshift_schema : str = None,
+        redshift_table : str = None,
+        s3_path : str = None,
+        mode : str = None,
+        *args, 
+        **kwargs
+    ):
+        """
+        This operator is designed to copy data from S3 into Redshift. It supports a replace mode that truncates the 
+        table before inserting data. This feature complements the default append behavior of the COPY SQL statements
+        
+        :param aws_conn_id:
+        :type aws_conn_id:
+        :param redshift_conn_id:
+        :type redshift_conn_id:
+        :param redshift_schema:
+        :type redshift_schema:
+        :param redshift_table:
+        :type redshift_table:
+        :param s3_path:
+        :type s3_path:
+        :param mode:
+        :type mode:
+        """
 
         super(StageToRedshiftOperator, self).__init__(*args, **kwargs)
-        # Map params here
-        # Example:
-        # self.conn_id = conn_id
+        self.aws_conn_id = aws_conn_id
+        self.redshift_conn_id = redshift_conn_id
+        self.redshift_schema = redshift_schema
+        self.redshift_table = redshift_table
+        self.s3_path = s3_path
+        self.mode = mode
 
-    def execute(self, context):
-        self.log.info('StageToRedshiftOperator not implemented yet')
 
+    def execute(
+        self, 
+        context
+    ):
+        
+        s3_hook = S3Hook(aws_conn_id=self.aws_conn_id)
+        redshift_hook = PostgresHook(postgres_conn_id=self.redshift_conn_id)
+        access_key = s3_hook.get_credentials().access_key
+        secret_key = s3_hook.get_credentials().secret_key
+       
+        
+        if self.mode == 'REPLACE':
+            # if method is set to REPLACE the table is truncated before inserting data
+            self.log.info(f"REPLACE method selected. Truncating Redshift table {self.redshift_schema}.{self.redshift_table}")
+            query_truncate = f"""TRUNCATE {self.redshift_schema}.{self.redshift_table};"""
+            #redshift_hook.run(query_truncate, autocommit=self.autocommit)
+    
+        self.log.info(f'Executing COPY query. Ingesting data into {self.redshift_schema}.{self.redshift_table}')
+        query_copy = f"""
+                COPY {self.redshift_schema}.{self.redshift_table}
+                FROM '{self.s3_path}'
+                with credentials
+                'aws_access_key_id={access_key};aws_secret_access_key={secret_key}';
+            """
+        self.log.info(query_copy)
+        #redshift_hook.run(query_copy, autocommit=self.autocommit)
+        self.log.info("COPY query completed")
 
 
 
